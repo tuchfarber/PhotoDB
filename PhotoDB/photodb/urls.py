d72447977c6d600f225d44db1ca6722a279421a9
Matt Tuchfarber <matt@tuchfarber.com>
2018-02-15 22:31:01 -0500

    Removed admin interface

--- PhotoDB/photodb/urls.py
+++ PhotoDB/photodb/urls.py
@@ -5,7 +5,6 @@ from rest_framework.schemas import get_schema_view
 schema_view = get_schema_view(title='PhotoDB API')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/v1/', include('photodb.apps.api.urls')),
     path('api-docs/', schema_view),
     path('api_auth/', include('rest_framework.urls')),
